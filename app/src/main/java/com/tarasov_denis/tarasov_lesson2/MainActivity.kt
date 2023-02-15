e0c9b36d30953884fb69878cefa10f41295e2b9a
nibelung-dt <97225915+nibelung-dt@users.noreply.github.com>
2023-02-15 14:17:05 +0300

    Тарасов_Занятие_3-1

--- app/src/main/java/com/tarasov_denis/tarasov_lesson2/MainActivity.kt
+++ app/src/main/java/com/tarasov_denis/tarasov_lesson2/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import com.tarasov_denis.tarasov_lesson2.SecondActivity.text.editText1
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val goToSecondActivity = findViewById<Button>(R.id.goTo)
-        val textView = findViewById<TextView>(R.id.textView)
-
-        if (editText1 != "") {
-            textView.setText(editText1)
-        }
 
         goToSecondActivity.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
@@ -26,3 +19,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+/*
+https://github.com/murgupluoglu/flagkit-android
+https://github.com/murgupluoglu/flagkit-android/blob/master/Flags.md
+
+
+ */
--- app/src/main/java/com/tarasov_denis/tarasov_lesson2/SecondActivity.kt
+++ app/src/main/java/com/tarasov_denis/tarasov_lesson2/SecondActivity.kt
@@ -5,30 +5,25 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import com.tarasov_denis.tarasov_lesson2.SecondActivity.text.editText1
+
+// import com.tarasov_denis.tarasov_lesson2.SecondActivity.text.editText1
 // import androidx.activity.result.ActivityResultLauncher
 // import androidx.activity.result.contract.ActivityResultContracts
 
 class SecondActivity : AppCompatActivity() {
 
-    object text {
-        var editText1 = ""
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val contract = CustomContract()
 
         val backToFirstActivity = findViewById<Button>(R.id.backToFirstActivity)
-        val editView = findViewById<EditText>(R.id.editView)
 
 
         backToFirstActivity.setOnClickListener {
-            editText1 = editView.getText().toString()
-            startActivity(contract.createIntent(this, editText1))
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
         }
 
     }
