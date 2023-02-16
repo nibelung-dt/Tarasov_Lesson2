cda53c1498acb0fccdc03cbf9bf5f99a6c3be0a4
nibelung-dt <97225915+nibelung-dt@users.noreply.github.com>
2023-02-17 02:50:10 +0300

    done
    done

--- app/src/main/java/com/tarasov_denis/tarasov_lesson2/MainActivity.kt
+++ app/src/main/java/com/tarasov_denis/tarasov_lesson2/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.tarasov_denis.tarasov_lesson2
 
-import android.content.Intent
+
+import android.graphics.Typeface
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
-import com.tarasov_denis.tarasov_lesson2.SecondActivity.text.editText1
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,16 +19,73 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val goToSecondActivity = findViewById<Button>(R.id.goTo)
-        val textView = findViewById<TextView>(R.id.textView)
+        val contacts = findViewById<Button>(R.id.contacts)
+        val vacancy = findViewById<Button>(R.id.vacancy)
+        val gallery = findViewById<Button>(R.id.gallery)
+        val offices = findViewById<TextView>(R.id.offices)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
 
-        if (editText1 != "") {
-            textView.setText(editText1)
+        contacts.setOnClickListener {
+            contacts.visibility = View.INVISIBLE
+            vacancy.visibility = View.INVISIBLE
+            gallery.visibility = View.INVISIBLE
+            offices.visibility = View.INVISIBLE
+            button1.visibility = View.INVISIBLE
+            button2.visibility = View.INVISIBLE
+            button3.visibility = View.INVISIBLE
+            button4.visibility = View.INVISIBLE
+            button5.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
         }
 
-        goToSecondActivity.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+        vacancy.setOnClickListener {
+            contacts.visibility = View.INVISIBLE
+            vacancy.visibility = View.INVISIBLE
+            gallery.visibility = View.INVISIBLE
+            offices.visibility = View.INVISIBLE
+            button1.visibility = View.INVISIBLE
+            button2.visibility = View.INVISIBLE
+            button3.visibility = View.INVISIBLE
+            button4.visibility = View.INVISIBLE
+            button5.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
         }
+
+        gallery.setOnClickListener {
+            contacts.visibility = View.INVISIBLE
+            vacancy.visibility = View.INVISIBLE
+            gallery.visibility = View.INVISIBLE
+            offices.visibility = View.INVISIBLE
+            button1.visibility = View.INVISIBLE
+            button2.visibility = View.INVISIBLE
+            button3.visibility = View.INVISIBLE
+            button4.visibility = View.INVISIBLE
+            button5.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
+        }
+
+        // Не разобрался, как добавить пробелы в spannable. "\n" не работает
+
+        // <b>ИТ-решения для\nразвития бизнеса</b>\nРазработка программного\nобеспечения
+        val header = findViewById<TextView>(R.id.header)
+        val spannable = SpannableString("ИТ-решения для развития бизнеса Разработка программного обеспечения")
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD),
+            20, // start
+            22, // end
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+       // header.text = spannable
+
+
+           // val intent = Intent(this, SecondActivity::class.java)
+           // startActivity(intent)
+
     }
 }
+
